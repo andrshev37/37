cb2ccc1591c146d2c65a35dd20bbf2356f2fb328
andrshev37 <79997127+andrshev37@users.noreply.github.com>
2021-04-29 12:21:23 +0300

    Add files via upload

--- samostina/samostina.cpp
+++ samostina/samostina.cpp
@@ -1,14 +1,33 @@
 ﻿#include <iostream>
 #include <string>
+#include <fstream>
+#include <algorithm>
+#include <cctype>
+using namespace std;
 
 void noalpha() {
     string word;
     while(cin>>word)
     {
-        
+        for (int i = 0; i < word.length(); i++) {
+            if (!isalpha(word[i])) {
+                cout << word[i];
+            }
+        }
     }
+}
+void second() {
+    string word,stt;
+    ifstream in("in.txt");
+    while (in >> word) {
+        word[0] = toupper(word[0]);
+        stt += word;
+    }
+    cout << stt;
+    in.close();
 }
 int main()
 {
-    
+    noalpha();
+    second();
 }
